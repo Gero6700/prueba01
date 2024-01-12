68082d6577a88d7b590db72e1842f954b2f17c7e
aolivares-shr <aolivares@senatorhr.com>
2024-01-12 13:35:45 +0100

    Move dtos to infra proyect

--- src/Infra/Providers/IAvailabilitySynchronizerApiClient.cs
+++ src/Infra/Providers/IAvailabilitySynchronizerApiClient.cs
@@ -4,5 +4,8 @@ namespace Senator.As400.Cloud.Sync.Infrastructure.Providers {
     public interface IAvailabilitySynchronizerApiClient {
         Task<Response> CreateContract(Contract contract);
         Task<Response> CreateContractClient(ContractClient contractClient);
+        Task<Response> UpdateContract(Contract contract);
+        Task<Response> UpdateContractClient(ContractClient contractClient);
+        Task<Response> DeleteContract(string codContract);
     }
 }
