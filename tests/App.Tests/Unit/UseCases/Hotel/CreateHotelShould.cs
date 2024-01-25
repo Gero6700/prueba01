7bc9cb00a5a0ff4ba83c095bcfd664bf4ad928c6
aolivares-shr <aolivares@senatorhr.com>
2024-01-25 13:46:41 +0100

    Red do_not_create_hotel_when_hotcod_is_zero

--- tests/App.Tests/Unit/UseCases/Hotel/CreateHotelShould.cs
+++ tests/App.Tests/Unit/UseCases/Hotel/CreateHotelShould.cs
@@ -34,6 +34,23 @@ public class CreateHotelShould {
             .CreateHotel(Arg.Is<Infrastructure.Dtos.BookingCenter.Hotel>(c => IsEquivalent(c, expectedHotel)));
     }
 
+    [Test]
+    public async Task do_not_create_hotel_when_hotcod_is_zero() {
+        //Given
+        var anyHotcod = 0;
+        var anyHozhor = "anyHozhor";
+        var anyReshotel = new Reshotel {
+            Hotcod = anyHotcod,
+            Hozhor = anyHozhor
+        };
+
+        //When
+        Func<Task> function = async () => await createHotel.Execute(anyReshotel);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid hotel code");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
