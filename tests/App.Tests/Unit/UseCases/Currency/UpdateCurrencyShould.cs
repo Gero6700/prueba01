a7fac15c51d65b976a92e3daf811d0a6bb39eef0
aolivares-shr <aolivares@senatorhr.com>
2024-01-26 12:56:33 +0100

    Red do_not_update_currency_when_dinomb2_is_invalid

--- tests/App.Tests/Unit/UseCases/Currency/UpdateCurrencyShould.cs
+++ tests/App.Tests/Unit/UseCases/Currency/UpdateCurrencyShould.cs
@@ -37,6 +37,26 @@ public class UpdateCurrencyShould {
             .UpdateCurrency(Arg.Is<Infrastructure.Dtos.BookingCenter.Currency>(c => IsEquivalent(c, expectedCurrency)));
     }
 
+    [Test]
+    public async Task do_not_update_currency_when_dinomb2_is_invalid() {
+        //Then
+        const string anyDinom2 = "EU0";
+        const string anyDinomb = "anyDinomb";
+        const string anyDisimb = "anyDisimb";
+
+        var anyDivisa = new Divisa {
+            Dinom2 = anyDinom2,
+            Dinomb = anyDinomb,
+            Disimb = anyDisimb
+        };
+
+        //When
+        Func<Task> function = async () => await updateCurrency.Execute(anyDivisa);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid iso code");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
