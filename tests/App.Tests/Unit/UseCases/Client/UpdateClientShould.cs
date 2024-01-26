02bfe6f1fdf438949c4c7ce01becdee39e737e56
aolivares-shr <aolivares@senatorhr.com>
2024-01-26 12:36:06 +0100

    Red do_not_update_client_when_idusuario_is_zero

--- tests/App.Tests/Unit/UseCases/Client/UpdateClientShould.cs
+++ tests/App.Tests/Unit/UseCases/Client/UpdateClientShould.cs
@@ -1,3 +1,5 @@
+using Senator.As400.Cloud.Sync.Application.UseCases.Client;
+
 namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.Client;
 
 [TestFixture]
@@ -43,6 +45,30 @@ public class UpdateClientShould {
         await availabilitySynchronizerApiClient.Received()
             .UpdateClient(Arg.Is<Infrastructure.Dtos.BookingCenter.Client>(c => IsEquivalent(c, expectedClient)));
 
+    }
+
+    [Test]
+    public async Task do_not_update_client_when_idusuario_is_zero() {
+        //Given
+        const long anyIdUsuario = 0;
+        const string anyUsuario = "anyUsuario";
+        const string anyClave = "anyClave";
+        const string anyNombreComercial = "anyNombreComercial";
+        const char anyAgGroup = 'A';
+
+        var anyUsureg = UsuregBuilder.AnUsuregBuilder()
+            .WithIdUsuario(anyIdUsuario)
+            .WithUsuario(anyUsuario)
+            .WithClave(anyClave)
+            .WithNombreComercial(anyNombreComercial)
+            .WithAgGroup(anyAgGroup)
+            .Build();
+
+        //When
+        Func<Task> function = async () => await updateClient.Execute(anyUsureg);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Incorrect user code");
     }
 
     private bool IsEquivalent(object source, object expected) {
