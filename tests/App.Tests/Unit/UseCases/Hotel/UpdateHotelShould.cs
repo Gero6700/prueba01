7a49ffd6d035906098a635762406f019180f5684
aolivares-shr <aolivares@senatorhr.com>
2024-01-26 12:41:02 +0100

    Red do_not_update_hotel_when_hotcod_is_zero

--- tests/App.Tests/Unit/UseCases/Hotel/UpdateHotelShould.cs
+++ tests/App.Tests/Unit/UseCases/Hotel/UpdateHotelShould.cs
@@ -1,3 +1,5 @@
+using Senator.As400.Cloud.Sync.Application.UseCases.Hotel;
+
 namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.Hotel;
 
 [TestFixture]
@@ -38,6 +40,23 @@ public class UpdateHotelShould {
         
         await availabilitySynchronizerApiClient.Received()
             .UpdateHotel(Arg.Is<Infrastructure.Dtos.BookingCenter.Hotel>(c => IsEquivalent(c, expectedHotel)));
+    }
+
+    [Test]
+    public async Task do_not_update_hotel_when_hotcod_is_zero() {
+        //Given
+        var anyHotcod = 0;
+        var anyHozhor = "anyHozhor";
+        var anyReshotel = new Reshotel {
+            Hotcod = anyHotcod,
+            Hozhor = anyHozhor
+        };
+
+        //When
+        Func<Task> function = async () => await updateHotel.Execute(anyReshotel);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid hotel code");
     }
 
     private bool IsEquivalent(object source, object expected) {
