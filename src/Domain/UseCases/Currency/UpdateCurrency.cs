a444d4d29c4806b2d7d26568034eabe3123f4a36
aolivares-shr <aolivares@senatorhr.com>
2024-01-26 12:58:34 +0100

    Green do_not_update_currency_when_dinomb2_is_invalid

--- src/Domain/UseCases/Currency/UpdateCurrency.cs
+++ src/Domain/UseCases/Currency/UpdateCurrency.cs
@@ -7,6 +7,10 @@ public class UpdateCurrency {
     }
 
     public async Task Execute(Divisa divisa) {
+        if (divisa.Dinom2.Length != 3 || !divisa.Dinom2.All(char.IsLetter)) {
+            throw new ArgumentException("Invalid iso code");
+        }
+
         var currency= divisa.ToCurrency();
         await availabilitySynchronizerApiClient.UpdateCurrency(currency);
         
