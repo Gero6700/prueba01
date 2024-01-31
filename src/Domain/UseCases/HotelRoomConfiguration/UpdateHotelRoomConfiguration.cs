2337974b82359e22db37b43021be819aed37da29
aolivares-shr <aolivares@senatorhr.com>
2024-01-31 12:04:30 +0100

    Green do_not_update_hotel_room_configuration_when_tihabg_is_empty

--- src/Domain/UseCases/HotelRoomConfiguration/UpdateHotelRoomConfiguration.cs
+++ src/Domain/UseCases/HotelRoomConfiguration/UpdateHotelRoomConfiguration.cs
@@ -13,6 +13,10 @@ public class UpdateHotelRoomConfiguration {
         if (resthaho.Tihab == "") {
             throw new ArgumentException("Incorrect room code");
         }
+        if (resthaho.Tihabg == "") {
+            throw new ArgumentException("Incorrect inventory room type code");
+        }
+
         var hotelRoomConfiguration = resthaho.ToHotelRoomConfiguration();
         await availabilitySynchronizerApiClient.UpdateHotelRoomConfiguration(hotelRoomConfiguration);
     }
