0c06621b62d54a499f0284a3507dff5599592a1b
aolivares-shr <aolivares@senatorhr.com>
2024-02-01 14:59:01 +0100

    Red update_market

---
+++ src/Domain/UseCases/Market/UpdateMarket.cs
@@ -0,0 +1,8 @@
+namespace Senator.As400.Cloud.Sync.Application.UseCases.Market;
+
+public class UpdateMarket {
+    public Task Execute(Merca merca) {
+      throw new NotImplementedException();
+    }
+    
+}
---
+++ tests/App.Tests/Unit/UseCases/Market/UpdateMarketShould.cs
@@ -0,0 +1,41 @@
+namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.Market;
+
+[TestFixture]
+public class UpdateMarketShould {
+    private IAvailabilitySynchronizerApiClient availabilitySynchronizerApiClient;
+    private UpdateMarket updateMarket;
+
+    [SetUp]
+    public void SetUp() {
+        availabilitySynchronizerApiClient = Substitute.For<IAvailabilitySynchronizerApiClient>();
+        updateMarket = new UpdateMarket();
+    }
+    [Test]
+    public async Task update_market() {
+        //Given
+        const string anyCod = "COD";
+        const string anyNom = "anyNom";
+        var anyMerca = new Merca {
+            Cod = anyCod,
+            Nom = anyNom
+        };
+
+        //When
+        await updateMarket.Execute(anyMerca);
+
+        //Then
+        var expectedMarket = new Infrastructure.Dtos.BookingCenter.Market {
+            Code = anyCod,
+            Description = anyNom
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .UpdateMarket(Arg.Is<Infrastructure.Dtos.BookingCenter.Market>(c => IsEquivalent(c, expectedMarket)));
+    }
+
+    private bool IsEquivalent(object source, object expected) {
+        source.Should().BeEquivalentTo(expected);
+        return true;
+    }
+
+}
