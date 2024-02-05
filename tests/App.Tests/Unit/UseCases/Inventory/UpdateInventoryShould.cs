c0792e4e9f06fbc977c6dd1baa826c84ebd4fe7d
aolivares-shr <aolivares@senatorhr.com>
2024-02-05 12:51:21 +0100

    Red  do_not_update_inventory_when_pthab_is_empty

--- tests/App.Tests/Unit/UseCases/Inventory/UpdateInventoryShould.cs
+++ tests/App.Tests/Unit/UseCases/Inventory/UpdateInventoryShould.cs
@@ -110,6 +110,35 @@ public class UpdateInventoryShould
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Incorrect hotel code");
     }
 
+    [Test]
+    public async Task do_not_update_inventory_when_pthab_is_empty() {
+        //Given
+        const int anyPtfec = 20240101;
+        const int anyPtcupo = 5;
+        const int anyPtbloq = 0;
+        const int anyPtreal = 0;
+        const int anyPtgrup = 0;
+        const int anyPtreag = 0;
+        const int anyPthot = 150;
+        const string anyPthab = "";
+        var anyResplaht = new Resplaht {
+            Ptfec = anyPtfec,
+            Ptcupo = anyPtcupo,
+            Ptbloq = anyPtbloq,
+            Ptreal = anyPtreal,
+            Ptgrup = anyPtgrup,
+            Ptreag = anyPtreag,
+            Pthot = anyPthot,
+            Pthab = anyPthab
+        };
+
+        // When
+        Func<Task> function = async () => await updateInventory.Execute(anyResplaht);
+
+        // Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Incorrect room code");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
