1338de71d846524384e239a23b4531172056bb50
aolivares-shr <aolivares@senatorhr.com>
2024-02-08 14:20:42 +0100

    Green create_extra_when_c5apdt_is_s

--- src/Infra/Extensions/ConextraExtension.cs
+++ src/Infra/Extensions/ConextraExtension.cs
@@ -15,7 +15,7 @@ public static class ConextraExtension {
             ApplyBy = conextra.C5foun == "D" ? ApplyStayPriceType.D : conextra.C5foun == "P" ? ApplyStayPriceType.P : conextra.C5foun == "X" ? ApplyStayPriceType.X : ApplyStayPriceType.U,
             Price = conextra.C5prec,
             PriceApplication = conextra.C5form == "D" ? ApplyStayPriceType.D : conextra.C5form == "P" ? ApplyStayPriceType.P : conextra.C5form == "X" ? ApplyStayPriceType.X : ApplyStayPriceType.U,
-            ApplyOtherSuplementsOrDiscounts = conextra.C5apdt =="C" ? ApplyOtherSuplementsOrDiscounts.Contract : ApplyOtherSuplementsOrDiscounts.All,
+            ApplyOtherSuplementsOrDiscounts = conextra.C5apdt =="C" ? ApplyOtherSuplementsOrDiscounts.Contract : conextra.C5apdt == "S" ? ApplyOtherSuplementsOrDiscounts.Offer : ApplyOtherSuplementsOrDiscounts.All,
             IsCancellationGuarantee = conextra.Cogc,
             OccupancyRateCod = conextra.C5cocu == 0 ? "" : conextra.C5cocu.ToString()
         };
