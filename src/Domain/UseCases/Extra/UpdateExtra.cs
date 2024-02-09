c3961d43f21c8cfb903f16c96ae06dba1aa2c034
aolivares-shr <aolivares@senatorhr.com>
2024-02-09 11:16:34 +0100

    Green do_not_update_extra_when_c5freh_is_invalid

--- src/Domain/UseCases/Extra/UpdateExtra.cs
+++ src/Domain/UseCases/Extra/UpdateExtra.cs
@@ -11,6 +11,9 @@ public class UpdateExtra {
         if (DateTimeHelper.ConvertJulianDateToDateTime(conextra.C5fred) == DateTime.MinValue) {
             throw new ArgumentException("Invalid apply from date");
         }
+        if (DateTimeHelper.ConvertJulianDateToDateTime(conextra.C5freh) == DateTime.MinValue) {
+            throw new ArgumentException("Invalid apply to date");
+        }
         var extra = conextra.ToExtra();
         await availabilitySynchronizerApiClient.UpdateExtra(extra);
     }
