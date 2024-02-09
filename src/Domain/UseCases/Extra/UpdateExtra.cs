89acefa521e7f08d5dcca60183fa91beca07eaf1
aolivares-shr <aolivares@senatorhr.com>
2024-02-09 10:57:20 +0100

    Red update_extra

---
+++ src/Domain/UseCases/Extra/UpdateExtra.cs
@@ -0,0 +1,7 @@
+namespace Senator.As400.Cloud.Sync.Application.UseCases.Extra;
+
+public class UpdateExtra {
+    public Task Execute(Conextra conextra) { 
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/App.Tests/Unit/UseCases/Extra/UpdateExtraShould.cs
@@ -0,0 +1,70 @@
+using Senator.As400.Cloud.Sync.Infrastructure.Dtos.As400;
+
+namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.Extra;
+
+[TestFixture]
+public class UpdateExtraShould {
+    private IAvailabilitySynchronizerApiClient availabilitySynchronizerApiClient;
+    private UpdateExtra updateExtra;
+
+    [SetUp]
+    public void SetUp() {
+        availabilitySynchronizerApiClient = Substitute.For<IAvailabilitySynchronizerApiClient>();
+        updateExtra = new UpdateExtra();
+    }
+
+    [Test]
+
+    public async Task update_extra() {
+        //Given
+        const int anyC5fred = 2024001;
+        const int anyC5freh = 2024366;
+        const int anyC5fec1 = 2024001;
+        const int anyC5fec2 = 2024366;
+        const string anyC5Sele = "";
+        const string anyC5foun = "U";
+        const string anyC5form = "U";
+        const string anyC5apdt = "";
+
+        var anyConextra = ConextraBuilder.AConextraBuilder()
+            .WithC5fred(anyC5fred)
+            .WithC5freh(anyC5freh)
+            .WithC5fec1(anyC5fec1)
+            .WithC5fec2(anyC5fec2)
+            .WithC5Sele(anyC5Sele)
+            .WithC5foun(anyC5foun)
+            .WithC5form(anyC5form)
+            .WithC5apdt(anyC5apdt)
+            .Build();
+
+        //When
+        await updateExtra.Execute(anyConextra);
+
+        //Then
+        var expectedExtra = new Infrastructure.Dtos.BookingCenter.Extra {
+            Code = anyConextra.Code,
+            ApplyFrom = new DateTime(2024, 01, 01),
+            ApplyTo = new DateTime(2024, 12, 31),
+            CheckInFrom = new DateTime(2024, 01, 01),
+            CheckInTo = new DateTime(2024, 12, 31),
+            StayFrom = anyConextra.C5died,
+            StayTo = anyConextra.C5dieh,
+            Mandatory = true,
+            Quantity = anyConextra.C5unid,
+            ByDay = anyConextra.C5inta,
+            ApplyBy = ApplyStayPriceType.U,
+            Price = anyConextra.C5prec,
+            PriceApplication = ApplyStayPriceType.U,
+            ApplyOtherSuplementsOrDiscounts = ApplyOtherSuplementsOrDiscounts.All,
+            IsCancellationGuarantee = anyConextra.Cogc,
+            OccupancyRateCod = anyConextra.C5cocu.ToString()
+        };
+        await availabilitySynchronizerApiClient.Received()
+            .CreateExtra(Arg.Is<Infrastructure.Dtos.BookingCenter.Extra>(x => IsEquivalent(x, expectedExtra)));
+    }
+
+    private bool IsEquivalent(object source, object expected) {
+        source.Should().BeEquivalentTo(expected);
+        return true;
+    }
+}
