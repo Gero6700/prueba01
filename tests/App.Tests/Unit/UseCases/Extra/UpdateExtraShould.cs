08951d5e202950cd495c060d1987bdca534aadd0
aolivares-shr <aolivares@senatorhr.com>
2024-02-09 11:17:22 +0100

    Red do_not_update_extra_when_c5fec1_is_invalid

--- tests/App.Tests/Unit/UseCases/Extra/UpdateExtraShould.cs
+++ tests/App.Tests/Unit/UseCases/Extra/UpdateExtraShould.cs
@@ -96,6 +96,22 @@ public class UpdateExtraShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid apply to date");
     }
 
+    [Test]
+    public async Task do_not_update_extra_when_c5fec1_is_invalid() {
+        // Given
+        const int anyC5fec1 = 202401;
+
+        var anyConextra = ConextraBuilder.AConextraBuilder()
+            .WithC5fec1(anyC5fec1)
+            .Build();
+
+        // When
+        Func<Task> function = async () => await updateExtra.Execute(anyConextra);
+
+        // Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid check-in from date");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
