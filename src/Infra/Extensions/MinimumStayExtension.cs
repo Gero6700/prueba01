7b0bc927640ce3ff0f553ecbbe4526f34446f57c
aolivares-shr <aolivares@senatorhr.com>
2024-02-19 12:57:26 +0100

    Green create_minimum_stay_when_c7peri_is_empty

--- src/Infra/Extensions/MinimumStayExtension.cs
+++ src/Infra/Extensions/MinimumStayExtension.cs
@@ -6,7 +6,7 @@ public static class MinimumStayExtension {
             From = DateTimeHelper.ConvertIntegerToDatetime(conestmi.C7fec1),
             To = DateTimeHelper.ConvertIntegerToDatetime(conestmi.C7fec2),
             Days = conestmi.C7dmin,
-            StrictPeriod = conestmi.C7peri == 'S',
+            StrictPeriod = conestmi.C7peri.ToString().ToUpper() == "S",
             ContractClientCode = conestmi.GetContractClientCode,
             RoomCode = conestmi.C7thab,
             RegimeCode = conestmi.C7regi
--- tests/App.Tests/Unit/UseCases/MinimumStay/CreateMinimumStayShould.cs
+++ tests/App.Tests/Unit/UseCases/MinimumStay/CreateMinimumStayShould.cs
@@ -47,6 +47,43 @@ public class CreateMinimumstayShould {
             CreateMinimumStay(Arg.Is<Infrastructure.Dtos.BookingCenter.MinimumStay>(x => IsEquivalent(x, expectedConestmi)));
     }
 
+    [Test]
+    public async Task create_minimum_stay_when_c7peri_is_empty() {
+        //Given
+        const int anyC7fec1 = 20240101;
+        const int anyC7fec2 = 20240101;
+        const int anyCofec1 = 2024001;
+        const char anyC7peri = '\0';
+
+        var anyConestmi = ConestmiBuilder.AConestmiBuilder()
+            .WithC7fec1(anyC7fec1)
+            .WithC7fec2(anyC7fec2)
+            .WithCofec1(anyCofec1)
+            .WithC7peri(anyC7peri)
+            .Build();
+
+        //When
+        await createMinimumStay.Execute(anyConestmi);
+
+        //Then
+        var expectedConestmi = new Infrastructure.Dtos.BookingCenter.MinimumStay {
+            Code = string.Concat(anyConestmi.C7hote, anyConestmi.C7cont, anyCofec1, anyConestmi.C7vers, 
+                       anyConestmi.C7agen,anyConestmi.C7sucu, anyConestmi.C7agcl, anyConestmi.C7sucl, anyConestmi.C7Lin),
+            From = new DateTime(2024, 01, 01),
+            To = new DateTime(2024, 01, 01),
+            Days = anyConestmi.C7dmin,
+            StrictPeriod = false,
+            ContractClientCode = string.Concat(anyConestmi.C7hote, anyConestmi.C7cont, anyCofec1, anyConestmi.C7vers,
+                       anyConestmi.C7agen, anyConestmi.C7sucu, anyConestmi.C7agcl, anyConestmi.C7sucl),
+            RoomCode = anyConestmi.C7thab,
+            RegimeCode = anyConestmi.C7regi
+        };
+
+        await availabilitySynchronizerApiClient.Received().
+            CreateMinimumStay(Arg.Is<Infrastructure.Dtos.BookingCenter.MinimumStay>(x => IsEquivalent(x, expectedConestmi)));
+
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
