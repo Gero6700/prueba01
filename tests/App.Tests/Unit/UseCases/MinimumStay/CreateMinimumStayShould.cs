14ae6d2a7793c5428a2a72b0b7b3f8d0f50c89c8
aolivares-shr <aolivares@senatorhr.com>
2024-02-19 13:14:37 +0100

    Red do_not_create_minimum_stary_when_c7fec2_is_invalid

--- tests/App.Tests/Unit/UseCases/MinimumStay/CreateMinimumStayShould.cs
+++ tests/App.Tests/Unit/UseCases/MinimumStay/CreateMinimumStayShould.cs
@@ -106,6 +106,28 @@ public class CreateMinimumstayShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid start date");
     }
 
+    [Test]
+    public async Task do_not_create_minimum_stary_when_c7fec2_is_invalid() {
+        //Given
+        const int anyC7fec1 = 20240101;
+        const int anyC7fec2 = 20240132;
+        const int anyCofec1 = 2024001;
+        const char anyC7peri = 'S';
+
+        var anyConestmi = ConestmiBuilder.AConestmiBuilder()
+            .WithC7fec1(anyC7fec1)
+            .WithC7fec2(anyC7fec2)
+            .WithCofec1(anyCofec1)
+            .WithC7peri(anyC7peri)
+            .Build();
+
+        //When
+        Func<Task> function = async () => await createMinimumStay.Execute(anyConestmi);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid end date");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
