5246ee101f63afefe17a3be1449dae8beef213ef
aolivares-shr <aolivares@senatorhr.com>
2024-02-21 11:49:15 +0100

    Red do_not_create_markup_when_mkcbwh_is_null

--- tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
+++ tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
@@ -78,6 +78,34 @@ public class CreateMarkupShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Booking window from is required");
     }
 
+    [Test]
+    public async Task do_not_create_markup_when_mkcbwh_is_null() {
+        //Given
+        const int anyMkcid = 1;
+        const int anyMkcfed = 20240601;
+        const int anyMkcfeh = 20240602;
+        const decimal anyMkccpor = 5;
+        var anyMkcgrb = new DateTime(2024, 1, 1);
+        var anyMkcbwd = new DateTime(2024, 1, 1);
+        var anyMkcbwh = DateTime.MinValue;
+
+        var anyMkupcabe = new Mkupcabe {
+            Mkcid = anyMkcid,
+            Mkcgrb = anyMkcgrb,
+            Mkcbwd = anyMkcbwd,
+            Mkcbwh = anyMkcbwh,
+            Mkcfed = anyMkcfed,
+            Mkcfeh = anyMkcfeh,
+            Mkccpor = anyMkccpor
+        };
+
+        //When
+        Func<Task> function = async () => await createMarkup.Execute(anyMkupcabe);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Booking window to is required");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
