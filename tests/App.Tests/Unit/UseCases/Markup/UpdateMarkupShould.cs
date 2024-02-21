55be921ae5cde97ef60a245a727afbfc8b046551
aolivares-shr <aolivares@senatorhr.com>
2024-02-21 14:35:19 +0100

    Red do_not_update_markup_when_mkcfed_is_invalid

--- tests/App.Tests/Unit/UseCases/Markup/UpdateMarkupShould.cs
+++ tests/App.Tests/Unit/UseCases/Markup/UpdateMarkupShould.cs
@@ -165,6 +165,34 @@ public class UpdateMarkupShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Stay date to is required");
     }
 
+    [Test]
+    public async Task do_not_update_markup_when_mkcfed_is_invalid() {
+        //Given
+        const int anyMkcid = 1;
+        const int anyMkcfed = 20240600;
+        const int anyMkcfeh = 20240602;
+        const decimal anyMkccpor = 5;
+        var anyMkcgrb = new DateTime(2024, 1, 1);
+        var anyMkcbwd = new DateTime(2024, 1, 1);
+        var anyMkcbwh = new DateTime(2024, 12, 31);
+
+        var anyMkupcabe = new Mkupcabe {
+            Mkcid = anyMkcid,
+            Mkcgrb = anyMkcgrb,
+            Mkcbwd = anyMkcbwd,
+            Mkcbwh = anyMkcbwh,
+            Mkcfed = anyMkcfed,
+            Mkcfeh = anyMkcfeh,
+            Mkccpor = anyMkccpor
+        };
+
+        //When
+        Func<Task> function = async () => await updateMarkup.Execute(anyMkupcabe);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Stay date from is invalid");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
