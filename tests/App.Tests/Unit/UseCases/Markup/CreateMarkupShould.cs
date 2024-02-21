c45928bff12d5e01d34813e9af0cb29e613bfd1e
aolivares-shr <aolivares@senatorhr.com>
2024-02-21 12:35:52 +0100

    Red do_not_create_markup_when_mkcfeh_is_less_than_mkcfed

--- tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
+++ tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
@@ -246,6 +246,34 @@ public class CreateMarkupShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Booking window to is less than boking window from");
     }
 
+    [Test]
+    public async Task do_not_create_markup_when_mkcfeh_is_less_than_mkcfed() {
+        //Given
+        const int anyMkcid = 1;
+        const int anyMkcfed = 20240602;
+        const int anyMkcfeh = 20240601;
+        const decimal anyMkccpor = 5;
+        var anyMkcgrb = new DateTime(2024, 1, 1);
+        var anyMkcbwd = new DateTime(2024, 1, 1);
+        var anyMkcbwh = new DateTime(2024, 12, 31);
+        
+        var anyMkupcabe = new Mkupcabe {
+            Mkcid = anyMkcid,
+            Mkcgrb = anyMkcgrb,
+            Mkcbwd = anyMkcbwd,
+            Mkcbwh = anyMkcbwh,
+            Mkcfed = anyMkcfed,
+            Mkcfeh = anyMkcfeh,
+            Mkccpor = anyMkccpor
+        };
+
+        //When
+        Func<Task> function = async () => await createMarkup.Execute(anyMkupcabe);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Stay date to is less than stay date from");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
