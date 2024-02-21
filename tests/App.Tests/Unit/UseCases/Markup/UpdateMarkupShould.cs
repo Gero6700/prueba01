a873015748b0c5049c6f10dd9aec509910ece810
aolivares-shr <aolivares@senatorhr.com>
2024-02-21 13:33:44 +0100

    Red do_not_update_markup_when_mkcbwd_is_null

--- tests/App.Tests/Unit/UseCases/Markup/UpdateMarkupShould.cs
+++ tests/App.Tests/Unit/UseCases/Markup/UpdateMarkupShould.cs
@@ -1,3 +1,5 @@
+using Senator.As400.Cloud.Sync.Application.UseCases.Markup;
+
 namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.Markup;
 [TestFixture]
 public class UpdateMarkupShould {
@@ -48,6 +50,34 @@ public class UpdateMarkupShould {
         await availabilitySynchronizerApiClient.Received()
             .UpdateMarkup(Arg.Is<Infrastructure.Dtos.BookingCenter.Markup>(x => IsEquivalent(x, expectedMarkup)));
 
+    }
+
+    [Test]
+    public async Task do_not_update_markup_when_mkcbwd_is_null() {
+        //Given
+        const int anyMkcid = 1;
+        const int anyMkcfed = 20240601;
+        const int anyMkcfeh = 20240602;
+        const decimal anyMkccpor = 5;
+        var anyMkcgrb = new DateTime(2024, 1, 1);
+        var anyMkcbwd = DateTime.MinValue;
+        var anyMkcbwh = new DateTime(2024, 12, 31);
+
+        var anyMkupcabe = new Mkupcabe {
+            Mkcid = anyMkcid,
+            Mkcgrb = anyMkcgrb,
+            Mkcbwd = anyMkcbwd,
+            Mkcbwh = anyMkcbwh,
+            Mkcfed = anyMkcfed,
+            Mkcfeh = anyMkcfeh,
+            Mkccpor = anyMkccpor
+        };
+
+        //When
+        Func<Task> function = async () => await updateMarkup.Execute(anyMkupcabe);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Booking window from is required");
     }
 
     private bool IsEquivalent(object source, object expected) {
