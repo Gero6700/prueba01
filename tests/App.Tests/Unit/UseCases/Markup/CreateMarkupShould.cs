a53d1a145bde63f70237d8ed0d7104e5b759b145
aolivares-shr <aolivares@senatorhr.com>
2024-02-21 11:40:14 +0100

    Red do_not_create_markup_when_mkcbwd_is_null

--- tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
+++ tests/App.Tests/Unit/UseCases/Markup/CreateMarkupShould.cs
@@ -50,6 +50,34 @@ public class CreateMarkupShould {
             .CreateMarkup(Arg.Is<Infrastructure.Dtos.BookingCenter.Markup>(x => IsEquivalent(x, expectedMarkup)));
     }
 
+    [Test]  
+    public async Task do_not_create_markup_when_mkcbwd_is_null() {
+        //Given
+        const int anyMkcid = 1;
+        const int anyMkcfed = 20240601;
+        const int anyMkcfeh = 20240602;
+        const decimal anyMkccpor = 5;
+        var anyMkcgrb = new DateTime(2024, 1, 1);
+        var anyMkcbwd = DateTime.MinValue;
+        var anyMkcbwh = new DateTime(2024, 12, 31);
+
+        var anyMkupcabe = new Mkupcabe {
+            Mkcid = anyMkcid,
+            Mkcgrb = anyMkcgrb,
+            Mkcbwd = anyMkcbwd,
+            Mkcbwh = anyMkcbwh,
+            Mkcfed = anyMkcfed,
+            Mkcfeh = anyMkcfeh,
+            Mkccpor = anyMkccpor
+        };
+
+        //When
+        Func<Task> function = async () => await createMarkup.Execute(anyMkupcabe);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Incorrect booking window from");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
