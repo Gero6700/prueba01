15688243ea245a089c8f7eb2f180a23c76d30ce8
aolivares-shr <aolivares@senatorhr.com>
2024-03-01 13:03:32 +0100

    Green do_not_update_cancellation_policy_line_when_c6fec1_is_invalid

--- src/Domain/UseCases/CancellationPolicyLine/UpdateCancellationPolicyLine.cs
+++ src/Domain/UseCases/CancellationPolicyLine/UpdateCancellationPolicyLine.cs
@@ -7,6 +7,9 @@ public class UpdateCancellationPolicyLine {
     }
 
     public async Task Execute(Congasan congasan) {
+        if (DateTimeHelper.ConvertIntegerToDatetime(congasan.C6fec1) == DateTime.MinValue) {
+            throw new ArgumentException("Invalid from date");
+        }
         var cancellationPolicyLine = congasan.ToCancellationPolicyLine();
 
         await availabilitySynchronizerApiClient.UpdateCancellationPolicyLine(cancellationPolicyLine);
