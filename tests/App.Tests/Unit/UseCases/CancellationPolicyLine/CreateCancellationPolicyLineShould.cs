ed3dcce9f2497a50c6dc22a6051d0f9707f05fe3
aolivares-shr <aolivares@senatorhr.com>
2024-03-01 10:19:46 +0100

    Red create_cancellation_policy_line_when_c6segu_is_selected

--- tests/App.Tests/Unit/UseCases/CancellationPolicyLine/CreateCancellationPolicyLineShould.cs
+++ tests/App.Tests/Unit/UseCases/CancellationPolicyLine/CreateCancellationPolicyLineShould.cs
@@ -1,3 +1,5 @@
+using Senator.As400.Cloud.Sync.Infrastructure.Dtos.As400;
+
 namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.CancellationPolicyLine;
 [TestFixture]
 public class CreateCancellationPolicyLineShould {
@@ -142,6 +144,49 @@ public class CreateCancellationPolicyLineShould {
             ApplicationType = CancellationPolicyApplicationType.FirstNight,
             ApplyInOfferPrice = true,
             ApplyIfInsurance = false,
+            RefundAsBonus = false,
+            ContractClients = new List<string>() { anyCongasan.OriginCode },
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .CreateCancellationPolicyLine(Arg.Is<Infrastructure.Dtos.BookingCenter.CancellationPolicyLine>(x => IsEquivalent(x, expectedCancellationPolicyLine)));
+
+    }
+
+    [Test]
+    public async Task create_cancellation_policy_line_when_c6segu_is_selected() {
+        //Given
+        const OriginType anyOriginType = OriginType.Contract;
+        const int anyC6fec1 = 20240101;
+        const int anyC6fec2 = 20241231;
+        const string anyC6segu = "S";
+        const string anyC6bono = "";
+
+        var anyCongasan = CongasanBuilder.ACongasanBuilder()
+            .WithOriginType(anyOriginType)
+            .WithC6fec1(anyC6fec1)
+            .WithC6fec2(anyC6fec2)            
+            .WithC6segu(anyC6segu)
+            .WithC6bono(anyC6bono)
+            .Build();
+
+        //When
+        await createCancellationPolicyLine.Execute(anyCongasan);
+
+        //Then
+        var expectedCancellationPolicyLine = new Infrastructure.Dtos.BookingCenter.CancellationPolicyLine {
+            Code = anyCongasan.Code,
+            From = new DateTime(2024, 01, 01),
+            To = new DateTime(2024, 12, 31),
+            ReleaseDays = anyCongasan.C6gcdi,
+            ReleaseHours = anyCongasan.C6gcho,
+            PenaltyNights = anyCongasan.C6gcno,
+            PenaltyPercent = anyCongasan.C6gcpo,
+            PenaltyAmount = anyCongasan.C6gcim,
+            ApplicationMargin = anyCongasan.C6marg,
+            ApplicationType = anyCongasan.C6medi.ToUpper() == "S" ? CancellationPolicyApplicationType.Avarage : CancellationPolicyApplicationType.FirstNight,
+            ApplyInOfferPrice = anyCongasan.C6ofer.ToUpper() == "S" ? true : false,
+            ApplyIfInsurance = true,
             RefundAsBonus = false,
             ContractClients = new List<string>() { anyCongasan.OriginCode },
         };
