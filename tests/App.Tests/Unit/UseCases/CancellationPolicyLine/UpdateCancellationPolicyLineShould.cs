27b4d8fd2efcd422789e413cbc2abf93617e155a
aolivares-shr <aolivares@senatorhr.com>
2024-03-01 13:04:16 +0100

    Red do_not_update_cancellation_policy_line_when_c6fec2_is_invalid

--- tests/App.Tests/Unit/UseCases/CancellationPolicyLine/UpdateCancellationPolicyLineShould.cs
+++ tests/App.Tests/Unit/UseCases/CancellationPolicyLine/UpdateCancellationPolicyLineShould.cs
@@ -73,6 +73,22 @@ public class UpdateCancellationPolicyLineShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid from date");
     }
 
+    [Test]
+    public async Task do_not_update_cancellation_policy_line_when_c6fec2_is_invalid() {
+        //Given
+        const int anyC6fec2 = 2024;
+
+        var anyCongasan = CongasanBuilder.ACongasanBuilder()
+            .WithC6fec2(anyC6fec2)
+            .Build();
+
+        //When
+        Func<Task> function = async () => await updateCancellationPolicyLine.Execute(anyCongasan);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Invalid to date");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
