de82457abc810cfc158b820954b1f5157b423cbe
aolivares-shr <aolivares@senatorhr.com>
2024-03-07 11:22:55 +0100

    Red create_offer_and_supplement_when_ofopci_is_s

--- tests/App.Tests/Unit/UseCases/OfferAndSupplement/CreateOfferAndSupplementShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplement/CreateOfferAndSupplementShould.cs
@@ -162,6 +162,81 @@ public class CreateOfferAndSupplementShould {
     }
 
 
+    [Test]
+    public async Task create_offer_and_supplement_when_ofopci_is_s() {
+        //Given
+        const int anyOffec = 2024001;
+        const int anyOffec2 = 2024002;
+        const string anyOfopci = "S";
+        const string anyOffode = "";
+        const int anyOfftop = 0;
+        const string anyOfTies = "";
+        const string anyOfadni = "";
+        const int anyOfgrbd = 2024001;
+        const int anyOfgrbh = 2024002;
+        const string anyOfdfac = " ";
+        const string anyOffore = "";
+        const string anyOffors = "";
+        const string anyOftidt = "";
+        const string anyOfsobr = "";
+        const string anyOfapli = "";
+        const string anyO4tipa = "ADULT1";
+        const string anyO4tdto = "";
+        var anyOfdae = new[] { 0.0m, 0.0m, 0.0m, 0.0m }; //Descuentos adulto/estancia
+        var anyOfdas = new[] { 0.0m, 0.0m, 0.0m, 0.0m }; //Descuentos adulto/regimen
+        var anyOfdne = new[] { 0.0m, 0.0m, 0.0m, 0.0m }; //Descuentos niño/estancia
+        var anyOfdns = new[] { 0.0m, 0.0m, 0.0m, 0.0m }; //Descuentos niño/servicio
+        var anyOfthab = new[] { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }; //Tipos de habitacion
+        var anyOftser = new[] { "", "", "", "", "" }; //Tipos de servicio
+
+        var anyConofege = ConofegeBuilder.AConofegeBuilder()
+            .WithOffec(anyOffec)
+            .WithOffec2(anyOffec2)
+            .WithOfopci(anyOfopci)
+            .WithOffode(anyOffode)
+            .WithOfftop(anyOfftop)
+            .WithOfties(anyOfTies)
+            .WithOfadni(anyOfadni)
+            .WithOfgrbd(anyOfgrbd)
+            .WithOfgrbh(anyOfgrbh)
+            .WithOfdfac(anyOfdfac)
+            .WithOffore(anyOffore)
+            .WithOffors(anyOffors)
+            .WithOftidt(anyOftidt)
+            .WithOfsobr(anyOfsobr)
+            .WithOfapli(anyOfapli)
+            .WithO4tipa(anyO4tipa)
+            .WithO4tdto(anyO4tdto)
+            .WithOfdae(anyOfdae)
+            .WithOfdas(anyOfdas)
+            .WithOfdne(anyOfdne)
+            .WithOfdns(anyOfdns)
+            .WithOfthab(anyOfthab)
+            .WithOftser(anyOftser)
+            .Build();
+
+        //When
+        await createOfferAndSupplement.Execute(anyConofege);
+
+        //Then
+        var expectedOfferAndSupplement = new Infrastructure.Dtos.BookingCenter.OfferAndSupplement {
+            Code = anyConofege.Code,
+            Type = OfferSupplementType.Offer,
+            ApplyFrom = new DateTime(2024, 01, 01),
+            ApplyTo = new DateTime(2024, 01, 02),
+            ApplyOrder = null,
+            DepositAmount = anyConofege.Ofdpto,
+            DepositType = PaymentType.Fixed,
+            DepositBeforeDate = null,
+            ModificationCostsAmount = anyConofege.Gmimpo,
+            Conditions = [],
+            Configurations = []
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .CreateOfferAndSupplement(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplement>(x => IsEquivalent(x, expectedOfferAndSupplement)));
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
