aa73fb19123725eaebcf5465147dab5ac3b763b9
aolivares-shr <aolivares@senatorhr.com>
2024-03-13 14:10:03 +0100

    Green do_not_create_offer_and_supplement_when_offec2_is_invalid

--- src/Domain/UseCases/OfferAndSupplement/CreateOfferAndSupplement.cs
+++ src/Domain/UseCases/OfferAndSupplement/CreateOfferAndSupplement.cs
@@ -10,6 +10,9 @@ public class CreateOfferAndSupplement {
         if (DateTimeHelper.ConvertJulianDateToDateTime(conofege.Offec) == DateTime.MinValue) {
             throw new ArgumentException("Invalid apply from date");
         }
+        if (DateTimeHelper.ConvertJulianDateToDateTime(conofege.Offec2) == DateTime.MinValue) {
+            throw new ArgumentException("Invalid apply to date");
+        }
 
         var offerAndSupplement = conofege.ToOfferAndSupplement();
         await availabilitySynchronizerApiClient.CreateOfferAndSupplement(offerAndSupplement);
