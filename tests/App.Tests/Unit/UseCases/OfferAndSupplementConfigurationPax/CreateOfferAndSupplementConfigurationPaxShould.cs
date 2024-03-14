c43ef80aedf22eff35f821ed5eb4406ff3752173
aolivares-shr <aolivares@senatorhr.com>
2024-03-14 14:27:56 +0100

    Greem create_offer_and_supplement_configuration_pax_when_o4dto_is_e

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementConfigurationPax/CreateOfferAndSupplementConfigurationPaxShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementConfigurationPax/CreateOfferAndSupplementConfigurationPaxShould.cs
@@ -89,6 +89,45 @@ public class CreateOfferAndSupplementConfigurationPaxShould {
             .CreateOfferAndSupplementConfigurationPax(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementConfigurationPax>(x => IsEquivalent(x, expectedOfferAndSupplementConfigurationPax)));
     }
 
+    [Test]
+    public async Task create_offer_and_supplement_configuration_pax_when_o4dto_is_e() {
+        //Given
+        const string anyO4tipa = "NIÑOS1";
+        const string anyO4dto = "E";
+        const decimal anyO4desd = 20.00m;
+        const decimal anyO4has = 21.99m;
+        const decimal anyO4dtos = 10.00m;
+        const string anyCode = "anyCode";
+        const string anyOfferAndSupplementCode = "anyOfferAndSupplementCode";
+
+        var condtof = new Condtof {
+            O4tipa = anyO4tipa,
+            O4tdto = anyO4dto,
+            O4desd = anyO4desd,
+            O4has = anyO4has,
+            O4dtos = anyO4dtos,
+            Code = anyCode,
+            OfferAndSupplementCode = anyOfferAndSupplementCode
+        };
+
+        //When
+        await createOfferAndSupplementConfigurationPax.Execute(condtof);
+
+        //Then
+        var expectedOfferAndSupplementConfigurationPax = new Infrastructure.Dtos.BookingCenter.OfferAndSupplementConfigurationPax {
+            PaxOrder = 1,
+            PaxType = PaxType.Child,
+            Scope = ScopeType.Stay,
+            AgeFrom = anyO4desd,
+            AgeTo = anyO4has,
+            Amount = anyO4dtos,
+            AmountType = PaymentType.Percent
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .CreateOfferAndSupplementConfigurationPax(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementConfigurationPax>(x => IsEquivalent(x, expectedOfferAndSupplementConfigurationPax)));
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
