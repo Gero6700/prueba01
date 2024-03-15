4036c1d3a8c81a1ddd9a95454e716f1fe97091df
aolivares-shr <aolivares@senatorhr.com>
2024-03-15 11:57:51 +0100

    Green create_offer_and_supplement_configuration_pax_when_o4tipa_is_adult_and_o4has_is_less_18

--- src/Infra/Extensions/CondtofExtension.cs
+++ src/Infra/Extensions/CondtofExtension.cs
@@ -1,9 +1,10 @@
 namespace Senator.As400.Cloud.Sync.Infrastructure.Extensions;
 public static class CondtofExtension {
     public static OfferAndSupplementConfigurationPax ToOfferAndSupplementConfigurationPax(this Condtof condtof) {
+        var paxTypeString = condtof.O4tipa[..5].ToUpper();
         return new OfferAndSupplementConfigurationPax {
             PaxOrder = int.Parse(condtof.O4tipa.Trim()[5..]),
-            PaxType = condtof.O4tipa[..5].ToUpper() == "ADULT" ? PaxType.Adult : condtof.O4tipa[..5].ToUpper() == "NIÑOS" ? PaxType.Child : PaxType.Adult,
+            PaxType = paxTypeString == "ADULT" && condtof.O4has < 18 ? PaxType.Teenager :  paxTypeString == "ADULT" ? PaxType.Adult : paxTypeString == "NIÑOS" ? PaxType.Child : PaxType.Adult,
             Scope = condtof.O4tdto.ToUpper() == "E" ? ScopeType.Stay : condtof.O4tdto.ToUpper() == "S" ? ScopeType.Regime : ScopeType.Stay,
             AgeFrom = condtof.O4desd,
             AgeTo = condtof.O4has,
