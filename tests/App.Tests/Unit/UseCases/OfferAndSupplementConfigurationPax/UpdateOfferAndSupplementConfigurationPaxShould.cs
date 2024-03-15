5de4adde223aa3ca98a171d3213b281616600661
aolivares-shr <aolivares@senatorhr.com>
2024-03-15 13:20:54 +0100

    Red do_not_update_offer_and_supplement_configuration_pax_when_o4tipa_is_empty

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementConfigurationPax/UpdateOfferAndSupplementConfigurationPaxShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementConfigurationPax/UpdateOfferAndSupplementConfigurationPaxShould.cs
@@ -195,6 +195,34 @@ public class UpdateOfferAndSupplementConfigurationPaxShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Age to is required");
     }
 
+    [Test]
+    public async Task do_not_update_offer_and_supplement_configuration_pax_when_o4tipa_is_empty() {
+        //Given
+        const string anyO4tipa = "";
+        const string anyO4dto = "";
+        const decimal anyO4desd = 20.00m;
+        const decimal anyO4has = 21.99m;
+        const decimal anyO4dtos = 10.00m;
+        const string anyCode = "anyCode";
+        const string anyOfferAndSupplementCode = "anyOfferAndSupplementCode";
+
+        var condtof = new Condtof {
+            O4tipa = anyO4tipa,
+            O4tdto = anyO4dto,
+            O4desd = anyO4desd,
+            O4has = anyO4has,
+            O4dtos = anyO4dtos,
+            Code = anyCode,
+            OfferAndSupplementCode = anyOfferAndSupplementCode
+        };
+
+        //When
+        Func<Task> function = async () => await updateOfferAndSupplementConfigurationPax.Execute(condtof);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Pax type is required");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
