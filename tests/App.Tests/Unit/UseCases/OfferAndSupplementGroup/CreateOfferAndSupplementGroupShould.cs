1ffd31cf43781ab898b5b4ec4df81ef7a76b7bd1
aolivares-shr <aolivares@senatorhr.com>
2024-03-18 14:24:57 +0100

    Red do_not_create_offer_and_supplement_group_when_ocfec2_is_less_than_ocfec1_and_they_are_not_zero

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/CreateOfferAndSupplementGroupShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/CreateOfferAndSupplementGroupShould.cs
@@ -92,6 +92,26 @@ public class CreateOfferAndSupplementGroupShould{
             .CreateOfferAndSupplementGroup(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroup>(x => IsEquivalent(x, expectedOfferAndSupplementGroup)));
     }
 
+    [Test]
+    public async Task do_not_create_offer_and_supplement_group_when_ocfec2_is_less_than_ocfec1_and_they_are_not_zero() {
+        //Given
+        const int anyOccin = 1;
+        const int anyOcfec1 = 20240102;
+        const int anyOcfec2 = 20240101;
+
+        var anyConofcomHeader = new ConofcomHeader {
+            Occin = anyOccin,
+            Ocfec1 = anyOcfec1,
+            Ocfec2 = anyOcfec2
+        };
+
+        //When
+        Func<Task> function = async () => await createOfferAndSupplementGroup.Execute(anyConofcomHeader);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Apply to date is less than apply from date");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
