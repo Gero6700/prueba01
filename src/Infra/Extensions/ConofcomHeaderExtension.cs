ac6641829c11daa6a76a659073262d9126f0dab1
aolivares-shr <aolivares@senatorhr.com>
2024-03-18 14:18:46 +0100

    Green create_offer_and_supplement_group_when_ocfec1_is_zero

--- src/Infra/Extensions/ConofcomHeaderExtension.cs
+++ src/Infra/Extensions/ConofcomHeaderExtension.cs
@@ -4,7 +4,7 @@ public static class ConofcomHeaderExtension{
     public static OfferAndSupplementGroup ToOfferAndSupplementGroup(this ConofcomHeader conofcomHeader) {
         return new OfferAndSupplementGroup {
             Code = conofcomHeader.Occin.ToString(),
-            ApplyFrom = DateTimeHelper.ConvertYYYYMMDDToDatetime(conofcomHeader.Ocfec1),
+            ApplyFrom = conofcomHeader.Ocfec1 == 0 ? DateTime.MinValue : DateTimeHelper.ConvertYYYYMMDDToDatetime(conofcomHeader.Ocfec1),
             ApplyTo = DateTimeHelper.ConvertYYYYMMDDToDatetime(conofcomHeader.Ocfec2)
         };
     }
--- tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/CreateOfferAndSupplementGroupShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/CreateOfferAndSupplementGroupShould.cs
@@ -38,6 +38,33 @@ public class CreateOfferAndSupplementGroupShould{
             .CreateOfferAndSupplementGroup(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroup>(x => IsEquivalent(x, expectedOfferAndSupplementGroup)));
     }
 
+    [Test]
+    public async Task create_offer_and_supplement_group_when_ocfec1_is_zero() {
+        //Given
+        const int anyOccin = 1;
+        const int anyOcfec1 = 0;
+        const int anyOcfec2 = 20240102;
+
+        var anyConofcomHeader = new ConofcomHeader {
+            Occin = anyOccin,
+            Ocfec1 = anyOcfec1,
+            Ocfec2 = anyOcfec2
+        };
+
+        //When
+        await createOfferAndSupplementGroup.Execute(anyConofcomHeader);
+
+        //Then
+        var expectedOfferAndSupplementGroup = new Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroup {
+            Code = "1",
+            ApplyFrom = DateTime.MinValue,
+            ApplyTo = new DateTime(2024, 1, 2)
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .CreateOfferAndSupplementGroup(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroup>(x => IsEquivalent(x, expectedOfferAndSupplementGroup)));
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
