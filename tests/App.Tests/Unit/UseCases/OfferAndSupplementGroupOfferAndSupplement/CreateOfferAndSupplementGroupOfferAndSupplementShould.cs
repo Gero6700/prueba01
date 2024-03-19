3607cbcea9bec2880374377c7e7598aefaeaf02f
aolivares-shr <aolivares@senatorhr.com>
2024-03-19 13:27:15 +0100

    Red do_not_create_offer_and_supplement_group_offer_and_supplement_when_occin_is_zero

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementGroupOfferAndSupplement/CreateOfferAndSupplementGroupOfferAndSupplementShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementGroupOfferAndSupplement/CreateOfferAndSupplementGroupOfferAndSupplementShould.cs
@@ -35,6 +35,24 @@ public class CreateOfferAndSupplementGroupOfferAndSupplementShould {
             .CreateOfferAndSupplementGroupOfferAndSupplement(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroupOfferAndSupplement>(x => IsEquivalent(x, expectedOfferAndSupplementGroupOfferAndSupplement)));
     }
 
+    [Test]
+    public async Task do_not_create_offer_and_supplement_group_offer_and_supplement_when_occin_is_zero() { 
+        //Given
+        const int anyOccin = 0;
+        const string anyOfferSupplementCode = "anyOfferSupplementCode";
+
+        var conofcomLine = new ConofcomLine {
+            Occin = anyOccin,
+            OfferSupCode = anyOfferSupplementCode
+        };
+
+        //When
+        Func<Task> function = async () => await createOfferAndSupplementGroupOfferAndSupplement.Execute(conofcomLine);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Group code is zero");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
