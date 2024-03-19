71a9bf07c5855709b116246ce0eb95bec81c2053
aolivares-shr <aolivares@senatorhr.com>
2024-03-19 13:28:03 +0100

    Green do_not_create_offer_and_supplement_group_offer_and_supplement_when_occin_is_zero

--- src/Domain/UseCases/OfferAndSupplementGroupOfferAndSupplement/CreateOfferAndSupplementGroupOfferAndSupplement.cs
+++ src/Domain/UseCases/OfferAndSupplementGroupOfferAndSupplement/CreateOfferAndSupplementGroupOfferAndSupplement.cs
@@ -7,8 +7,11 @@ public class CreateOfferAndSupplementGroupOfferAndSupplement {
     }
 
     public async Task Execute(ConofcomLine conofcomLine) {
-        var offerAndSupplementGroupOfferAndSupplement = conofcomLine.ToOfferAndSupplementGroupOfferAndSupplement();
+        if (conofcomLine.Occin == 0) {
+            throw new ArgumentException("Group code is zero");
+        }
 
+        var offerAndSupplementGroupOfferAndSupplement = conofcomLine.ToOfferAndSupplementGroupOfferAndSupplement();
         await availabilitySynchronizerApiClient.CreateOfferAndSupplementGroupOfferAndSupplement(offerAndSupplementGroupOfferAndSupplement);
     }
 }
