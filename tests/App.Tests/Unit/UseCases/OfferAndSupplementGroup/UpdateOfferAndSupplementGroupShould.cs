31abcbbb31fd08c801aecaf087e4d9b82d6759ca
aolivares-shr <aolivares@senatorhr.com>
2024-03-19 11:52:56 +0100

    Red do_not_update_offer_and_supplement_group_when_ocfec2_is_less_than_ocfec1_and_they_are_not_zero

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/UpdateOfferAndSupplementGroupShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/UpdateOfferAndSupplementGroupShould.cs
@@ -1,3 +1,5 @@
+using Senator.As400.Cloud.Sync.Application.UseCases.OfferAndSupplementGroup;
+
 namespace Senator.As400.Cloud.Sync.App.Tests.Unit.UseCases.OfferAndSupplementGroup;
 [TestFixture]
 public class UpdateOfferAndSupplementGroupShould {
@@ -35,6 +37,26 @@ public class UpdateOfferAndSupplementGroupShould {
 
         await availabilitySynchronizerApiClient.Received()
             .UpdateOfferAndSupplementGroup(Arg.Is<Infrastructure.Dtos.BookingCenter.OfferAndSupplementGroup>(x => IsEquivalent(x, expectedOfferAndSupplementGroup)));
+    }
+
+    [Test]
+    public async Task do_not_update_offer_and_supplement_group_when_ocfec2_is_less_than_ocfec1_and_they_are_not_zero() {
+        //Given
+        const int anyOccin = 1;
+        const int anyOcfec1 = 20240102;
+        const int anyOcfec2 = 20240101;
+
+        var anyConofcomHeader = new ConofcomHeader {
+            Occin = anyOccin,
+            Ocfec1 = anyOcfec1,
+            Ocfec2 = anyOcfec2
+        };
+
+        //When
+        Func<Task> function = async () => await updateOfferAndSupplementGroup.Execute(anyConofcomHeader);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Apply to date is less than apply from date");
     }
 
     private bool IsEquivalent(object source, object expected) {
