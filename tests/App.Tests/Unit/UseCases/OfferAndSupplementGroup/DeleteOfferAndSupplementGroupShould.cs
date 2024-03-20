0a366e24da74a6e74e35f03b31e35496f672839e
aolivares-shr <aolivares@senatorhr.com>
2024-03-20 13:38:14 +0100

    Red do_not_delete_offer_and_supplement_group_when_occin_is_zero

--- tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/DeleteOfferAndSupplementGroupShould.cs
+++ tests/App.Tests/Unit/UseCases/OfferAndSupplementGroup/DeleteOfferAndSupplementGroupShould.cs
@@ -27,6 +27,23 @@ public class DeleteOfferAndSupplementGroupShould {
             .DeleteOfferAndSupplementGroup(Arg.Is<String>(x => IsEquivalent(x, expectedCode)));
     }
 
+    [Test]
+    public async Task do_not_delete_offer_and_supplement_group_when_occin_is_zero() {
+        //Given
+        const int anyOccin = 0;
+
+        var conofcomHeader = new ConofcomHeader {
+            Occin = anyOccin
+        };
+        
+        //When
+        Func<Task> function = async () => await deleteOfferAndSupplementGroup.Execute(conofcomHeader);
+
+        //Then
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Group code is zero");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
