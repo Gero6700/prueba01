153b7d077d93dc4bca57946e69c51748a8c30f2e
aolivares-shr <aolivares@senatorhr.com>
2024-03-22 14:00:13 +0100

    Red do_not_create_period_pricing_when_c4esta_is_zero

--- tests/App.Tests/Unit/UseCases/PeriodPricing/CreatePeriodPricingShould.cs
+++ tests/App.Tests/Unit/UseCases/PeriodPricing/CreatePeriodPricingShould.cs
@@ -593,6 +593,42 @@ public class CreatePeriodPricingShould {
         await act.Should().ThrowAsync<ArgumentException>().WithMessage("Rate code is required");
     }
 
+    [Test]
+    public async Task do_not_create_period_pricing_when_c4esta_is_zero() {
+        //Given
+        const string anyRateCode = "anyRateCode";
+        const int anyCffec = 2024001;
+        const string anyContractClientCode = "anyContractClientCode";
+        const decimal anyC4esta = 0;
+        const string anyC4form = "";
+        const decimal anyC4serv = 9.99m;
+        const string anyC4fors = "";
+        const string anyC4thab = "anyC4thab";
+        const string anyC4tser = "anyC4tser";
+        const string anyRerele = "";
+        const int anyAcrele = 0;
+
+        var anyConpreci = new Conpreci {
+            RateCode = anyRateCode,
+            Cffec = anyCffec,
+            ContractClientCode = anyContractClientCode,
+            C4esta = anyC4esta,
+            C4form = anyC4form,
+            C4serv = anyC4serv,
+            C4fors = anyC4fors,
+            C4thab = anyC4thab,
+            C4tser = anyC4tser,
+            Rerele = anyRerele,
+            Acrele = anyAcrele
+        };
+
+        //When
+        Func<Task> act = async () => await createPeriodPricing.Execute(anyConpreci);
+
+        //Then
+        await act.Should().ThrowAsync<ArgumentException>().WithMessage("Stay price must be greater than zero");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
