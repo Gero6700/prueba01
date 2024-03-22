67ab98af4b2414ffb06a695056ffa056a8b2acb0
aolivares-shr <aolivares@senatorhr.com>
2024-03-22 13:06:45 +0100

    Green do_not_create_period_pricing_when_c4tser_is_empty

--- src/Domain/UseCases/PeriodPricing/CreatePeriodPricing.cs
+++ src/Domain/UseCases/PeriodPricing/CreatePeriodPricing.cs
@@ -16,6 +16,9 @@ public class CreatePeriodPricing {
         if (string.IsNullOrWhiteSpace(conpreci.C4thab)) {
             throw new ArgumentException("Room code is required");
         }
+        if (string.IsNullOrWhiteSpace(conpreci.C4tser)) {
+            throw new ArgumentException("Regime code is required");
+        }
         var periodPricing = conpreci.ToPeriodPricing();
 
         return availabilitySynchronizerApiClient.CreatePeriodPricing(periodPricing);
