8bf756f5582eeafe5045b22a2a434c529ddef343
aolivares-shr <aolivares@senatorhr.com>
2024-03-25 10:55:29 +0100

    Red do_not_update_period_pricing_when_cffec_is_zero

--- tests/App.Tests/Unit/UseCases/PeriodPricing/UpdatePeriodPricingShould.cs
+++ tests/App.Tests/Unit/UseCases/PeriodPricing/UpdatePeriodPricingShould.cs
@@ -65,6 +65,42 @@ public class UpdatePeriodPricingShould {
             .UpdatePeriodPricing(Arg.Is<Infrastructure.Dtos.BookingCenter.PeriodPricing>(x => IsEquivalent(x, expectedPeriodPricing)));
     }
 
+    [Test]
+    public async Task do_not_update_period_pricing_when_cffec_is_zero() {
+        //Given
+        const string anyRateCode = "anyRateCode";
+        const int anyCffec = 0;
+        const string anyContractClientCode = "anyContractClient";
+        const decimal anyC4esta = 24.99m;
+        const string anyC4form = "";
+        const decimal anyC4serv = 9.99m;
+        const string anyC4fors = "";
+        const string anyC4thab = "anyC4thab";
+        const string anyC4tser = "anyC4tser";
+        const string anyRerele = "";
+        const int anyAcrele = 0;
+
+        var anyConpreci = new Conpreci {
+            RateCode = anyRateCode,
+            Cffec = anyCffec,
+            ContractClientCode = anyContractClientCode,
+            C4esta = anyC4esta,
+            C4form = anyC4form,
+            C4serv = anyC4serv,
+            C4fors = anyC4fors,
+            C4thab = anyC4thab,
+            C4tser = anyC4tser,
+            Rerele = anyRerele,
+            Acrele = anyAcrele
+        };
+
+        //When
+        Func<Task> act = async () => await updatePeriodPricing.Execute(anyConpreci);
+
+        //Then
+        await act.Should().ThrowAsync<ArgumentException>().WithMessage("Price date is required");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
