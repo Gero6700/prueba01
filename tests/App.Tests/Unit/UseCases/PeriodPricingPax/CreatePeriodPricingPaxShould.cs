37c926a32520edcb8dc48d875f1525f994dd2320
aolivares-shr <aolivares@senatorhr.com>
2024-03-26 12:23:17 +0100

    Red do_not_create_period_pricing_pax_when_period_pricing_code_is_empty

--- tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
+++ tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
@@ -244,6 +244,34 @@ public class CreatePeriodPricingPaxShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Code is required");
     }
 
+    [Test]
+    public async Task do_not_create_period_pricing_pax_when_period_pricing_code_is_empty() {
+        //Given
+        const string anyD4tipa = "ADULT1";
+        const string anyD4tdto = "";
+        const decimal anyD4desd = 20.00m;
+        const decimal anyD4has = 21.99m;
+        const decimal anyD4dtos = 10.00m;
+        const string anyCode = "anyCode";
+        const string anyPeriodPricingCode = "";
+
+        var condtos = new Condtos {
+            Code = anyCode,
+            D4tipa = anyD4tipa,
+            D4tdto = anyD4tdto,
+            D4desd = anyD4desd,
+            D4has = anyD4has,
+            D4dtos = anyD4dtos,
+            PeriodPricingCode = anyPeriodPricingCode
+        };
+
+        //When
+        Func<Task> function = async () =>  await createPeriodPricingPax.Execute(condtos);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Period pricing code is required");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
