065fcf191c6923f585477b69b6a309e6c432114f
aolivares-shr <aolivares@senatorhr.com>
2024-03-26 09:54:51 +0100

    Red create_period_pricing_pax_when_d4tipa_is_adult_and_d4has_is_less_than_18

--- tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
+++ tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
@@ -93,6 +93,47 @@ public class CreatePeriodPricingPaxShould {
             .CreatePeriodPricingPax(Arg.Is<Infrastructure.Dtos.BookingCenter.PeriodPricingPax>(x => IsEquivalent(x, expectedPeriodPricingPax)));
     }
 
+    [Test]
+    public async Task create_period_pricing_pax_when_d4tipa_is_adult_and_d4has_is_less_than_18() {
+        //Given
+        const string anyD4tipa = "ADULT1";
+        const string anyD4tdto = "";
+        const decimal anyD4desd = 13.00m;
+        const decimal anyD4has = 17.99m;
+        const decimal anyD4dtos = 10.00m;
+        const string anyCode = "anyCode";
+        const string anyPeriodPricingCode = "anyPeriodPricingCode";
+
+        var condtos = new Condtos {
+            Code = anyCode,
+            D4tipa = anyD4tipa,
+            D4tdto = anyD4tdto,
+            D4desd = anyD4desd,
+            D4has = anyD4has,
+            D4dtos = anyD4dtos,
+            PeriodPricingCode = anyPeriodPricingCode
+        };
+
+        //When
+        await createPeriodPricingPax.Execute(condtos);
+
+        //Then
+        var expectedPeriodPricingPax = new Infrastructure.Dtos.BookingCenter.PeriodPricingPax {
+            PaxOrder = 1,
+            PaxType = PaxType.Teenager,
+            Scope = ScopeType.Stay,
+            AgeFrom = anyD4desd,
+            AgeTo = anyD4has,
+            Amount = anyD4dtos,
+            AmountType = PaymentType.Percent,
+            PeriodPricingCode = anyPeriodPricingCode,
+            Code = anyCode
+        };
+
+        await availabilitySynchronizerApiClient.Received()
+            .CreatePeriodPricingPax(Arg.Is<Infrastructure.Dtos.BookingCenter.PeriodPricingPax>(x => IsEquivalent(x, expectedPeriodPricingPax)));
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
