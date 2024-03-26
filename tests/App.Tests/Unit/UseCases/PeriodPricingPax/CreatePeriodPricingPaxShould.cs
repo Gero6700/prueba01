83c88c13e69cee16e1bced7795c66a3c67410282
aolivares-shr <aolivares@senatorhr.com>
2024-03-26 13:03:29 +0100

    Red do_not_create_period_pricing_pax_when_d4has_is_less_than_d4desd

--- tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
+++ tests/App.Tests/Unit/UseCases/PeriodPricingPax/CreatePeriodPricingPaxShould.cs
@@ -412,6 +412,34 @@ public class CreatePeriodPricingPaxShould {
         await function.Should().ThrowAsync<ArgumentException>().WithMessage("Age to is required");
     }
 
+    [Test]
+    public async Task do_not_create_period_pricing_pax_when_d4has_is_less_than_d4desd() {
+        //Given
+        const string anyD4tipa = "ADULT1";
+        const string anyD4tdto = "";
+        const decimal anyD4desd = 21.00m;
+        const decimal anyD4has = 20.99m;
+        const decimal anyD4dtos = 10.00m;
+        const string anyCode = "anyCode";
+        const string anyPeriodPricingCode = "anyPeriodPricingCode";
+
+        var condtos = new Condtos {
+            Code = anyCode,
+            D4tipa = anyD4tipa,
+            D4tdto = anyD4tdto,
+            D4desd = anyD4desd,
+            D4has = anyD4has,
+            D4dtos = anyD4dtos,
+            PeriodPricingCode = anyPeriodPricingCode
+        };
+
+        //When
+        Func<Task> function = async () =>  await createPeriodPricingPax.Execute(condtos);
+
+        //Then
+        await function.Should().ThrowAsync<ArgumentException>().WithMessage("Age to is less than age from");
+    }
+
     private bool IsEquivalent(object source, object expected) {
         source.Should().BeEquivalentTo(expected);
         return true;
