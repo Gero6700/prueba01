054f2e0bb2d436b350b04670554132df4c3fe68d
aolivares-shr <aolivares@senatorhr.com>
2024-04-01 13:28:15 +0200

    Green do_not_create_ocuppancy_rate_when_cmaxad_is_less_than_cminad

--- src/Domain/UseCases/OcuppanceRate/CreateOcuppancyRate.cs
+++ src/Domain/UseCases/OcuppanceRate/CreateOcuppancyRate.cs
@@ -10,6 +10,9 @@ public class CreateOcuppancyRate {
         if (string.IsNullOrWhiteSpace(resthaco.Cocod)) {
             throw new ArgumentException("Code is required");
         }
+        if (resthaco.Cmaxad < resthaco.Cminad) {
+            throw new ArgumentException("Max adult is less than min adult");
+        }
         var occuppancyRate = resthaco.ToOcuppancyRate();
         await availabilitySynchronizerApiClient.CreateOcuppancyRate(occuppancyRate);
     }
