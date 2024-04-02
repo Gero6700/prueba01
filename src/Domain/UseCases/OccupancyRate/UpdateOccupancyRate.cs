aff8bac25881164cb581109ee468e4547b982bee
aolivares-shr <aolivares@senatorhr.com>
2024-04-02 10:24:23 +0200

    Green do_not_update_occupancy_rate_when_cmaxad_is_less_than_cminad

--- src/Domain/UseCases/OccupancyRate/UpdateOccupancyRate.cs
+++ src/Domain/UseCases/OccupancyRate/UpdateOccupancyRate.cs
@@ -10,6 +10,9 @@ public class UpdateOccupancyRate {
         if (string.IsNullOrWhiteSpace(resthaco.Cocod)) {
             throw new ArgumentException("Code is required");
         }
+        if (resthaco.Cmaxad < resthaco.Cminad) {
+            throw new ArgumentException("Max adult is less than min adult");
+        }
         var occuppancyRate = resthaco.ToOccupancyRate();
         await availabilitySynchronizerApiClient.UpdateOccupancyRate(occuppancyRate);
     }
