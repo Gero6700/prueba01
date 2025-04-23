f85b88be569e968298d58f00e7b678ce6fce53e2
aolivares <aolivares@senatorhr.com>
2025-04-23 09:38:48 +0200

    modified methods names api

--- src/Infra/Providers/AvailabilitySynchronizerApiClient.cs
+++ src/Infra/Providers/AvailabilitySynchronizerApiClient.cs
@@ -9,186 +9,186 @@ public class AvailabilitySynchronizerApiClient : IAvailabilitySynchronizerApiCli
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateCancellationPolicy(CancellationPolicyDto cancellationPolicy) {
-        return httpClient.PostAsJsonAsync("api/v1/CancellationPolicy/create", cancellationPolicy);
+        return httpClient.PostAsJsonAsync("api/v1/cancellationpolicy/create", cancellationPolicy);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateCommission(CommissionDto commission) {
-        return httpClient.PostAsJsonAsync("api/v1/Commission/create", commission);
+        return httpClient.PostAsJsonAsync("api/v1/commission/create", commission);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateContractHeader(ContractHeaderDto contractHeader) {
-        return httpClient.PostAsJsonAsync("api/v1/ContractHeader/create", contractHeader);
+        return httpClient.PostAsJsonAsync("api/v1/contractheader/create", contractHeader);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateExtra(ExtraDto extra) {
-        return httpClient.PostAsJsonAsync("api/v1/Extra/create", extra);
+        return httpClient.PostAsJsonAsync("api/v1/extra/create", extra);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateHotel(HotelDto hotel) {
-        return httpClient.PostAsJsonAsync("api/v1/Hotel/create", hotel);
+        return httpClient.PostAsJsonAsync("api/v1/hotel/create", hotel);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateIntegration(IntegrationDto integration) {
-        return httpClient.PostAsJsonAsync("api/v1/Integration/create", integration);
+        return httpClient.PostAsJsonAsync("api/v1/integration/create", integration);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateIntegrationClientType(IntegrationClientTypeDto integrationClientType) {
-        return httpClient.PostAsJsonAsync("api/v1/IntegrationClientType/create", integrationClientType);
+        return httpClient.PostAsJsonAsync("api/v1/integrationclienttype/create", integrationClientType);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateIntegrationContract(IntegrationContractDto integrationContract) {
-        return httpClient.PostAsJsonAsync("api/v1/IntegrationContract/create", integrationContract);
+        return httpClient.PostAsJsonAsync("api/v1/integrationcontract/create", integrationContract);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateInventory(InventoryDto inventory) {
-        return httpClient.PostAsJsonAsync("api/v1/Inventory/create", inventory);
+        return httpClient.PostAsJsonAsync("api/v1/inventory/create", inventory);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateMarket(MarketDto market) {
-        return httpClient.PostAsJsonAsync("api/v1/Market/create", market);
+        return httpClient.PostAsJsonAsync("api/v1/market/create", market);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateMeal(MealDto meal) {
-        return httpClient.PostAsJsonAsync("api/v1/Meal/create", meal);
+        return httpClient.PostAsJsonAsync("api/v1/meal/create", meal);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateMinimumStay(MinimumStayDto minimumStay) {
-        return httpClient.PostAsJsonAsync("api/v1/MinimumStay/create", minimumStay);
+        return httpClient.PostAsJsonAsync("api/v1/minimumstay/create".ToLower(), minimumStay);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateOccupancyRate(OccupancyRateDto occupancyRate) {
-        return httpClient.PostAsJsonAsync("api/v1/OccupancyRate/create", occupancyRate);
+        return httpClient.PostAsJsonAsync("api/v1/occupancyrate/create", occupancyRate);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateOfferSupplement(OfferSupplementDto offerSupplement) {
-        return httpClient.PostAsJsonAsync("api/v1/OfferSupplement/create", offerSupplement);
+        return httpClient.PostAsJsonAsync("api/v1/offersupplement/create", offerSupplement);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateOfferSupplementConfigurationPax(OfferSupplementConfigurationPaxDto offerSupplementConfigurationPax) {
-        return httpClient.PostAsJsonAsync("api/v1/OfferSupplementConfigurationPax/create", offerSupplementConfigurationPax);
+        return httpClient.PostAsJsonAsync("api/v1/offersupplementconfigurationpax/create", offerSupplementConfigurationPax);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateOfferSupplementGroup(OfferSupplementGroupDto offerSupplementGroup) {
-        return httpClient.PostAsJsonAsync("api/v1/OfferSupplementGroup/create", offerSupplementGroup);
+        return httpClient.PostAsJsonAsync("api/v1/offersupplementgroup/create", offerSupplementGroup);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateOfferSupplementGroupRelation(OfferSupplementGroupRelationDto offerSupplementGroupRelation) {
-        return httpClient.PostAsJsonAsync("api/v1/OfferSupplementGroupRelation/create", offerSupplementGroupRelation);
+        return httpClient.PostAsJsonAsync("api/v1/offersupplementgrouprelation/create", offerSupplementGroupRelation);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreatePeriodPricing(PeriodPricingDto periodPricing) {
-        return httpClient.PostAsJsonAsync("api/v1/PeriodPricing/create", periodPricing);
+        return httpClient.PostAsJsonAsync("api/v1/periodpricing/create", periodPricing);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreatePeriodPricingPax(PeriodPricingPaxDto periodPricingPax) {
-        return httpClient.PostAsJsonAsync("api/v1/PeriodPricingPax/create", periodPricingPax);
+        return httpClient.PostAsJsonAsync("api/v1/periodpricingpax/create", periodPricingPax);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.CreateRoom(RoomDto room) {
-        return httpClient.PostAsJsonAsync("api/v1/Room/create", room);
+        return httpClient.PostAsJsonAsync("api/v1/room/create", room);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteCommission(string code) {
-        return httpClient.DeleteAsync($"api/v1/Commission/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/commission/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteIntegration(string code) {
-        return httpClient.DeleteAsync($"api/v1/Integration/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/integration/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteIntegrationClientType(string code) {
-        return httpClient.DeleteAsync($"api/v1/IntegrationClientType/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/integrationclienttype/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteMinimunStay(string code) {
-        return httpClient.DeleteAsync($"api/v1/MinimumStay/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/minimumstay/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteOfferSupplementConfigurationPax(string code) {
-        return httpClient.DeleteAsync($"api/v1/OfferSupplementConfigurationPax/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/offersupplementconfigurationpax/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeleteOfferSupplementGroupRelation(OfferSupplementGroupRelationDto offerSupplementGroupRelation) {
-        return httpClient.DeleteAsync($"api/v1/OfferSupplementGroupRelation/delete/{offerSupplementGroupRelation}");
+        return httpClient.DeleteAsync($"api/v1/offersupplementgrouprelation/delete/{offerSupplementGroupRelation}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.DeletePeriodPricingPax(string code) {
-        return httpClient.DeleteAsync($"api/v1/PeriodPricingPax/delete/{code}");
+        return httpClient.DeleteAsync($"api/v1/periodpricingpax/delete/{code}");
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.PushHotelRoomConfiguration(HotelRoomConfigurationDto hotelRoomConfiguration) {
-        return httpClient.PostAsJsonAsync("api/v1/HotelRoomConfiguration/push", hotelRoomConfiguration);
+        return httpClient.PostAsJsonAsync("api/v1/hotelroomconfiguration/push", hotelRoomConfiguration);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.PushHotelSeasons(HotelSeasonsDto hotelSeasons) {
-        return httpClient.PostAsJsonAsync("api/v1/HotelSeason/push", hotelSeasons);
+        return httpClient.PostAsJsonAsync("api/v1/hotelseason/push", hotelSeasons);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.PushOccupancyScore(OccupancyScoreDto occupancyScore) {
-        return httpClient.PostAsJsonAsync("api/v1/OccupancyScore/push", occupancyScore);
+        return httpClient.PostAsJsonAsync("api/v1/occupancyscore/push", occupancyScore);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateCancellationPolicy(CancellationPolicyDto cancellationPolicy) {
-        return httpClient.PutAsJsonAsync("api/v1/CancellationPolicy/update", cancellationPolicy);
+        return httpClient.PutAsJsonAsync("api/v1/cancellationpolicy/update", cancellationPolicy);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateCommission(CommissionDto commission) {
-        return httpClient.PutAsJsonAsync("api/v1/Commission/update", commission);   
+        return httpClient.PutAsJsonAsync("api/v1/commission/update", commission);   
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateContractHeader(ContractHeaderDto contractHeader) {
-        return httpClient.PutAsJsonAsync("api/v1/ContractHeader/update", contractHeader);
+        return httpClient.PutAsJsonAsync("api/v1/contractheader/update", contractHeader);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateExtra(ExtraDto extra) {
-        return httpClient.PutAsJsonAsync("api/v1/Extra/update", extra);
+        return httpClient.PutAsJsonAsync("api/v1/extra/update", extra);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateHotel(HotelDto hotel) {
-        return httpClient.PutAsJsonAsync("api/v1/Hotel/update", hotel);
+        return httpClient.PutAsJsonAsync("api/v1/hotel/update", hotel);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateIntegration(IntegrationDto integration) {
-        return httpClient.PutAsJsonAsync("api/v1/Integration/update", integration);
+        return httpClient.PutAsJsonAsync("api/v1/integration/update", integration);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateIntegrationClientType(IntegrationClientTypeDto integrationClientType) {
-        return httpClient.PutAsJsonAsync("api/v1/IntegrationClientType/update", integrationClientType);
+        return httpClient.PutAsJsonAsync("api/v1/integrationclienttype/update", integrationClientType);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateIntegrationContract(IntegrationContractDto integrationContract) {
-        return httpClient.PutAsJsonAsync("api/v1/IntegrationContract/update", integrationContract);
+        return httpClient.PutAsJsonAsync("api/v1/integrationcontract/update", integrationContract);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateInventory(InventoryDto inventory) {
-        return httpClient.PutAsJsonAsync("api/v1/Inventory/update", inventory);
+        return httpClient.PutAsJsonAsync("api/v1/inventory/update", inventory);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateMinimumStay(MinimumStayDto minimumStay) {
-        return httpClient.PutAsJsonAsync("api/v1/MinimumStay/update", minimumStay);
+        return httpClient.PutAsJsonAsync("api/v1/minimumstay/update".ToLower(), minimumStay);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateOccupancyRate(OccupancyRateDto occupancyRate) {
-        return httpClient.PutAsJsonAsync("api/v1/OccupancyRate/update", occupancyRate);
+        return httpClient.PutAsJsonAsync("api/v1/occupancyrate/update", occupancyRate);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateOfferSupplement(OfferSupplementDto offerSupplement) {
-        return httpClient.PutAsJsonAsync("api/v1/OfferSupplement/update", offerSupplement);
+        return httpClient.PutAsJsonAsync("api/v1/offersupplement/update", offerSupplement);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateOfferSupplementConfigurationPax(OfferSupplementConfigurationPaxDto offerSupplementConfigurationPax) {
-        return httpClient.PutAsJsonAsync("api/v1/OfferSupplementConfigurationPax/update", offerSupplementConfigurationPax);
+        return httpClient.PutAsJsonAsync("api/v1/offersupplementconfigurationpax/update", offerSupplementConfigurationPax);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdateOfferSupplementGroup(OfferSupplementGroupDto offerSupplementGroup) {
-        return httpClient.PutAsJsonAsync("api/v1/OfferSupplementGroup/update", offerSupplementGroup);
+        return httpClient.PutAsJsonAsync("api/v1/offersupplementgroup/update", offerSupplementGroup);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdatePeriodPricing(PeriodPricingDto periodPricing) {
-        return httpClient.PutAsJsonAsync("api/v1/PeriodPricing/update", periodPricing);
+        return httpClient.PutAsJsonAsync("api/v1/periodpricing/update", periodPricing);
     }
 
     Task<HttpResponseMessage> IAvailabilitySynchronizerApiClient.UpdatePeriodPricingPax(PeriodPricingPaxDto periodPricingPax) {
-        return httpClient.PutAsJsonAsync("api/v1/PeriodPricingPax/update", periodPricingPax);
+        return httpClient.PutAsJsonAsync("api/v1/periodpricingpax/update", periodPricingPax);
     }
 }
